449bc387a6f91e0bb964875f53913dedb60e2a25
Ava-TU <C24384981@myTUDublin.ie>
2024-12-02 00:17:19 +0000

    added pitch and volume slider

--- musicinstrumenttoy/ButtonAudio.gd
+++ musicinstrumenttoy/ButtonAudio.gd
@@ -14,6 +14,16 @@ func _process(delta: float) -> void:
 func _on_pressed() -> void:
 	%Button.play()
 	print("1")
-	%Sprite2D.texture = load("res://wallpaper1.jpg")
+	%Sprite2D.texture = load("res://wallpaper1.jpg") # changes sprite by loading in another image
 	await get_tree().create_timer(1.0).timeout
 	%Sprite2D.texture = load("res://icon.svg")
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	%Button.pitch_scale = value
+	pass # Replace with function body.
+
+
+func _on_volume_value_changed(value: float) -> void:
+	%Button.volume_db = value
+	pass # Replace with function body.
