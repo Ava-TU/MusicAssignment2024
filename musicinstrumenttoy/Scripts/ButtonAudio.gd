977309ad32c472d28f2575a28cd234c1a33219d8
Ava-TU <C24384981@myTUDublin.ie>
2024-12-08 18:16:56 +0000

    FULLSCREEN WORKING OMG

--- musicinstrumenttoy/Scripts/ButtonAudio.gd
+++ musicinstrumenttoy/Scripts/ButtonAudio.gd
@@ -6,6 +6,7 @@ var bus_index: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_viewport().size = DisplayServer.screen_get_size()
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
