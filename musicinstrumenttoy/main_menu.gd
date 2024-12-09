75da68e20931ad7a120af6eb1431fc2f15c17484
Ava-TU <C24384981@myTUDublin.ie>
2024-12-09 01:47:46 +0000

    Play Button works

--- musicinstrumenttoy/main_menu.gd
+++ musicinstrumenttoy/main_menu.gd
@@ -1,12 +1,27 @@
 extends Node2D
 
+@onready var play: Button = %Play
+@onready var quit: Button = %Quit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_viewport().size = DisplayServer.screen_get_size()
-	pass # Replace with function body.
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if %Play.is_hovered():
+		%PlayHover.texture = load("res://Sprites/Well Yes! Hover.png")
+	else:
+		%PlayHover.texture = load("")
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Music.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
 	pass
